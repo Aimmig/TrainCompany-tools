ded10a2fce4da292c15f6a09d9ddf0fcf247057d
Andre Immig <an_drimg@protonmail.com>
2025-09-11 13:12:14 +0200

    More sensible default equipments

--- validation/__init__.py
+++ validation/__init__.py
@@ -187,8 +187,10 @@ def validate(tc_directory: PathLike | str = '..',
             path['group'] = 0
         if 'electrified' not in path:
             path['electrified'] = True
-        if 'neededEquipments' not in path:
-            path['neededEquipments'] = ["DE"]
+        if not 'neededEquipments' in path:
+            path['neededEquipments'] = []
+        if set(path['neededEquipments']).isdisjoint(set(country_equipments)):
+            path['neededEquipments'].append('DE')
         if set(path['neededEquipments']).isdisjoint(set(gauge_equipments)):
             path['neededEquipments'].append('1435mm')
         # 2.0. has speed and int lenght > 1
